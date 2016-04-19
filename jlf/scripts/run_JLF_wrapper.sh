c64ae74ce37fc0debaaa8e5b913274932a94cc67
Adon Rosen <adrose@users.noreply.github.com>
2016-04-19 17:05:09 -0400

    afgr uploaded jlf job submission scripts
    via online web gui

---
+++ jlf/scripts/run_JLF_wrapper.sh
@@ -0,0 +1,6 @@
+subjlist=${1}
+subj=$(cat $subjlist | sed -n "${SGE_TASK_ID}p")
+
+/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/jlf/scripts/antsJLF_OASIS30CustomSubset_afgr.pl /data/joy/BBL/studies/pnc/processedData/structural/antsCorticalThickness/${subj}/ExtractedBrain0N4.nii.gz /data/joy/BBL/studies/pnc/processedData/structural/jlf/${subj}/jlf 1 0 Younger24
+
+
