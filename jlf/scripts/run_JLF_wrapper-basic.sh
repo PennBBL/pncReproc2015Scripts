a6c53c89d75ecf067b0b9dc65b5569576231de48
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-08-05 15:17:56 -0400

    afgr rm'ed scripts dir

--- jlf/scripts/run_JLF_wrapper-basic.sh
+++
@@ -1,9 +0,0 @@
-subjlist=${1}
-subj=$(cat $subjlist | sed -n "${SGE_TASK_ID}p")
-
-if [ ! -f /data/joy/BBL/studies/pnc/processedData/structural/jlf/${subj}/jlfLabels.nii.gz ] ; then
-  /data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/jlf/scripts/antsJLF_OASIS30CustomSubset_afgr.pl /data/joy/BBL/studies/pnc/processedData/structural/antsCorticalThickness/${subj}/ExtractedBrain0N4.nii.gz /data/joy/BBL/studies/pnc/processedData/structural/jlf/${subj}/jlf 1 1 Younger24
-else
-  echo "All Done"
-  exit 0 
-fi
