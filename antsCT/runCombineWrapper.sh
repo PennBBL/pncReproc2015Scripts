4d33a4a03970dfe4d6413d0e1daeb451722500d8
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-10-03 16:42:54 -0400

    afgr updated antsCT scripts directory with post proc script

--- antsCT/runCombineWrapper.sh
+++ antsCT/runCombineWrapper.sh
@@ -5,14 +5,14 @@ bblid=`echo ${subj} | cut -f 1 -d ,`
 scanid=`echo ${subj} | cut -f 2 -d ,`
 dateid=`echo ${subj} | cut -f 3 -d ,`
 antsPath="/data/joy/BBL/studies/pnc/processedData/structural/antsCorticalThickness/${bblid}/${dateid}x${scanid}/"
-outImg="GmCombinedPriors.nii.gz"
-parcImg=`ls /data/joy/BBL/studies/pnc/processedData/structural/mars/labeledImages/${bblid}_${dateid}x${scanid}*`
-parcDir="MARS"
+outImg="atropos3class.nii.gz"
+parcImg=`ls /data/joy/BBL/studies/pnc/processedData/structural/jlf/${bblid}/${dateid}x${scanid}/${bblid}_${dateid}x${scanid}_jlfLabels.nii.gz`
+parcDir="JLF"
 scriptToCall=""
 templateImg="/data/joy/BBL/studies/pnc/template/pnc_template_brain.nii.gz"
 if [ -z ${parcImg} ] ; then 
   echo "${bblid},${scanid},${dateid}" >> ${noParcText}  
 else
-  /data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/antsCT/combinePriors2-4-5.sh -d ${antsPath} -o ${outImg} -p ${parcImg} -P ${parcDir} -t ${templateImg} ; 
+  /data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/antsCT/antsCTPostProcAndGMD.sh -d ${antsPath} -o ${outImg} -p ${parcImg} -P ${parcDir} -t ${templateImg} ; 
 fi
   
