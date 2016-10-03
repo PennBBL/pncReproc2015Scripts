6310092f28d00e939bec28d5d1ed82ca97989240
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-10-03 16:21:01 -0400

    afgr cleaned up jlf call directory

--- jlf/procScripts/submitWrapper_basic-q-testing-20160525.sh
+++
@@ -1,4 +0,0 @@
-joblist="/home/arosen/tempCohortListSplit/jlfPaths/submitPaths/individualCohorts0${1}"
-ntasks=$(cat ${joblist} | wc -l)
-
-qsub -q basic.q -binding linear:2 -l h_vmem=1.5G,s_vmem=1.0G -S /bin/bash -e /data/joy/BBL/projects/pncReproc2015/jlf/errorLogs/ -o /data/joy/BBL/projects/pncReproc2015/jlf/outputLogs/ -t 1-${ntasks} /data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/jlf/scripts/run_JLF_wrapper-basic-testing-20160525.sh ${joblist}
--- jlf/procScripts/submitWrapper_replication.sh
+++
@@ -1,4 +0,0 @@
-joblist="/home/arosen/tempCohortListSplit/jlfPaths/submitPaths/jlfReplicationCohort.csv"
-ntasks=$(cat ${joblist} | wc -l)
-
-qsub -l h_vmem=6.5G,s_vmem=6.0G -S /bin/bash -e /data/joy/BBL/projects/pncReproc2015/jlf/errorLogs/ -o /data/joy/BBL/projects/pncReproc2015/jlf/outputLogs/ -t 1-${ntasks} /data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/jlf/scripts/run_JLF_wrapper_replication.sh ${joblist}
