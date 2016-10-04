31a91e5c2297297f952eec43fd9f8bfb66a30d8b
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-10-04 10:32:17 -0400

    afgr changed cohort submit paths

--- antsCT/submitWrapper.sh
+++ antsCT/submitWrapper.sh
@@ -1,4 +1,4 @@
-joblist="/home/arosen/tempCohortListSplit/cohort_list.csv"
+joblist="/data/joy/BBL/projects/pncReproc2015/antsCT/submitPaths/individualCohorts0"
 ntasks=$(cat ${joblist} | wc -l)
 
 qsub -q all.q,basic.q -l h_vmem=1.9G,s_vmem=1.5G -S /bin/bash -t 1-${ntasks} /data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/antsCT/runCombineWrapper.sh ${joblist}
