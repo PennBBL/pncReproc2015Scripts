48ea373901c4a5750346e41a064cee81b63f0900
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-10-12 13:39:53 -0400

    afgr updated antsCT runCombine for glasser parc

--- antsCT/runCombineWrapper.sh
+++ antsCT/runCombineWrapper.sh
@@ -6,13 +6,13 @@ scanid=`echo ${subj} | cut -f 2 -d ,`
 dateid=`echo ${subj} | cut -f 3 -d ,`
 antsPath="/data/joy/BBL/studies/pnc/processedData/structural/antsCorticalThickness/${bblid}/${dateid}x${scanid}/"
 outImg="atropos3class.nii.gz"
-parcImg=`ls /data/joy/BBL/studies/pnc/processedData/structural/jlf/${bblid}/${dateid}x${scanid}/${bblid}_${dateid}x${scanid}_jlfLabels.nii.gz`
+parcImg="/data/joy/BBL/applications/xcpEngine/networks/180GlasserPNCWithHemispheres.nii.gz"
 parcDir="JLF"
 scriptToCall=""
 templateImg="/data/joy/BBL/studies/pnc/template/pnc_template_brain.nii.gz"
 if [ -z ${parcImg} ] ; then 
   echo "${bblid},${scanid},${dateid}" >> ${noParcText}  
 else
-  /data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/antsCT/antsCTPostProcAndGMD.sh -d ${antsPath} -o ${outImg} -p ${parcImg} -P ${parcDir} -t ${templateImg} ; 
+  /data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/antsCT/antsCTPostProcAndGMD.sh -d ${antsPath} -o ${outImg} -p ${parcImg} -P ${parcDir} -t ${templateImg} -s 1; 
 fi
   
