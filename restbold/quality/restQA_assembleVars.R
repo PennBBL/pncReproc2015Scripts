803e53e2d57913a62b64284ddd1e28d638a4c305
Rastko Ciric <rciric@users.noreply.github.com>
2016-10-17 11:16:11 -0400

    Update restQA_assembleVars.R

--- restbold/quality/restQA_assembleVars.R
+++ restbold/quality/restQA_assembleVars.R
@@ -9,12 +9,12 @@
 ###################################################################
 
 data <- read.csv('/data/joy/BBL/studies/pnc/subjectData/n1601_go1_datarel_020716.csv')
-xcp <- read.csv('/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/restbold/quality/REST_XCP.csv')
-b0 <- read.csv('/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/dico/n1601_b0map_nos.csv')
+xcp <- read.csv('/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts_mv_20161007/restbold/quality/REST_XCP.csv')
+b0 <- read.csv('/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts_mv_20161007/dico/n1601_b0map_nos.csv')
 coverage <- read.csv('REST_COVERAGE_PROCESSED.csv')
-data2 <- merge(data,coverage,by='scanid',all.x=T)
-data2 <- merge(data2,b0,by='scanid')
-data2 <- merge(data2,xcp,by='scanid')
+data0 <- merge(data,coverage,by='scanid',all.x=T)
+data1 <- merge(data0,b0,by='scanid',all.x=T)
+data2 <- merge(data1,xcp,by='scanid',all.x=T)
 
 ###################################################################
 # Determine exclusion criteria
@@ -34,7 +34,7 @@ restQA <- data.frame(
    scanid=data2$scanid,
    restExclude=data2$restExclude,
    restExcludeVoxelwise=data2$restExcludeVoxelwise,
-   restNoDataExclude=!data2$restAcquired,
+   restNoDataExclude=as.numeric(!data2$restAcquired),
    restRelMeanRMSMotion=data2$relMeanRMSmotion,
    restRelMeanRMSMotionExclude=data2$restMeanRelRMSMotionExclude,
    restNSpikesMotion=data2$nframesHighMotionrms0.25,
@@ -46,6 +46,11 @@ restQA <- data.frame(
    restVoxelwiseCoverageExclude=!data2$restVoxelwiseCoverageInclude,
    restRpsMapCorrectionNotApplied=data2$B0MapUsable
 )
+
+newSeq=c(7287,7288,7289,7296,7297,7298,7308,7315)
+for (i in newSeq) {
+   restQA$restNoDataExclude[which(data2$scanid==i)] = 0
+}
 
 write.csv(restQA,'REST_QA.csv',row.names=F)
 
