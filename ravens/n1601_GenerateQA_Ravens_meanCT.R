a6f090b8f6156a08263df9b06bb360762d46bf79
Angel Garcia de la Garza <agarza@chead.uphs.upenn.edu>
2016-11-04 14:28:34 -0400

    All RAVENS Scripts

--- ravens/n1601_GenerateQA_Ravens_meanCT.R
+++ ravens/n1601_GenerateQA_Ravens_meanCT.R
@@ -1,7 +1,7 @@
 
 #Read Data
-data <- read.csv("/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts_mv_20161007/ravens/N1601_Ravens_MeanCT.csv")
-names(data) <- c("bblid","scanid","meanGM")
+data <- read.csv("/data/joy/BBL/projects/pncReproc2015/ravens/N1601_Ravens_MeanCT.csv")
+names(data) <- c("bblid","scanid","meanGM","spaCorr")
 
 
 ### Missing Ravens 
@@ -17,6 +17,15 @@ mean.MeanCT <- mean(data$meanGM, na.rm=T)
 
 data$flagQA_meanCT[which(data$meanGM > (mean.MeanCT + 2.5*sd.MeanCT) | data$meanGM < (mean.MeanCT -2.5*sd.MeanCT))] <- 1
 
-data$finalQA_ravens <- data$flagQA_meanCT + data$missingRavens
+## Flag 2.5 SD below correlation
 
-write.csv(data, "/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts_mv_20161007/ravens/N1601_Ravens_QA.csv", row.names=F)
+data$flagQA_Corr <- 0
+sd.Corr <- sd(data$spaCorr, na.rm = T)
+mean.Corr <- mean(data$spaCorr, na.rm=T)
+data$flagQA_Corr[which(data$spaCorr < (mean.Corr -2.5*sd.Corr))] <- 1
+
+
+data$finalQA_ravens <- data$flagQA_meanCT + data$missingRavens + data$flagQA_Corr
+data$finalQA_ravens[which(data$finalQA_ravens == 2)] <- 1
+
+write.csv(data, "/data/joy/BBL/projects/pncReproc2015/ravens/N1601_Ravens_QA.csv", row.names=F)
