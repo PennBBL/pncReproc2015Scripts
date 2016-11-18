89efe61fbde9b4d879efa6f378168c13c002f860
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-11-18 09:41:10 -0500

    afgr uploaded JLF and CT seg union scripts

---
+++ antsCT/submitWrapperUnion.sh
@@ -0,0 +1,4 @@
+joblist="/home/arosen/tempCohortListSplit/cohort_list.csv"
+ntasks=$(cat ${joblist} | wc -l)
+
+qsub -q all.q,basic.q -l h_vmem=1G,s_vmem=1G -S /bin/bash -t 1-${ntasks} /home/arosen/pncReproc2015Scripts/antsCT/runCombineWrapperUnion.sh ${joblist}
