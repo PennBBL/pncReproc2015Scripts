738d0f9da67017ec9057c0b0d4489e5b18c6c3f4
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-11-21 17:00:06 -0500

    afgr made edits to the JLF submission scripts

--- jlf/procScripts/submitWrapper.sh
+++ jlf/procScripts/submitWrapper.sh
@@ -1,4 +1,4 @@
-joblist="/home/arosen/tempCohortListSplit/jlfPaths/submitPaths/individualCohorts0${1}"
+joblist="/data/joy/BBL/projects/pncReproc2015/jlf/cohortList/individualCohorts0${1}"
 ntasks=$(cat ${joblist} | wc -l)
 
-qsub -l h_vmem=6.5G,s_vmem=6.0G -S /bin/bash -q all.q -e /data/joy/BBL/projects/pncReproc2015/jlf/errorLogs/ -o /data/joy/BBL/projects/pncReproc2015/jlf/outputLogs/ -t 1-${ntasks} /data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/jlf/scripts/run_JLF_wrapper.sh ${joblist}
+qsub -l h_vmem=6.5G,s_vmem=6.0G -S /bin/bash -q all.q,basic.q -e /data/joy/BBL/projects/pncReproc2015/jlf/errorLogs/ -o /data/joy/BBL/projects/pncReproc2015/jlf/outputLogs/ -t 1-${ntasks} /home/arosen/pncReproc2015Scripts/jlf/procScripts/run_JLF_wrapper.sh ${joblist}
