cb7c1e437f8ebaf59a5d2879052e9266651c6a48
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-11-22 15:36:07 -0500

    afgr edited script to work with ants and jlf intersection volume values

--- jlf/makeLobeSegmentations/runCombineWrapper.sh
+++ jlf/makeLobeSegmentations/runCombineWrapper.sh
@@ -3,6 +3,6 @@ subj=$(cat $subjlist | sed -n "${SGE_TASK_ID}p")
 bblid=`echo ${subj} | cut -f 1 -d ,`
 scanid=`echo ${subj} | cut -f 2 -d ,`
 dateid=`echo ${subj} | cut -f 3 -d ,`
-inputImage="/data/joy/BBL/studies/pnc/processedData/structural/jlf/${bblid}/${dateid}x${scanid}/${bblid}_${dateid}x${scanid}_jlfLabels.nii.gz"
+inputImage="/data/joy/BBL/studies/pnc/processedData/structural/jlf/${bblid}/${dateid}x${scanid}/${bblid}_${dateid}x${scanid}_jlfLabelsANTsCTUnion.nii.gz"
 outputImage="/data/joy/BBL/studies/pnc/processedData/structural/jlf/${bblid}/${dateid}x${scanid}/${bblid}_${dateid}x${scanid}_jlfLobularSegmentaion.nii.gz"
 /home/arosen/pncReproc2015Scripts/jlf/makeLobeSegmentations/oasisLabelsToLobar.sh ${inputImage} ${outputImage}
