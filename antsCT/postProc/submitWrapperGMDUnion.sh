9fa10e5e3935331b2f3026d791f1b62d85dd9197
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-11-28 14:51:34 -0500

    afgr updated paths for submission scripts

--- antsCT/postProc/submitWrapperGMDUnion.sh
+++ antsCT/postProc/submitWrapperGMDUnion.sh
@@ -1,4 +1,4 @@
 joblist="/home/arosen/tempCohortListSplit/cohort_list.csv"
 ntasks=$(cat ${joblist} | wc -l)
 
-qsub -q all.q,basic.q -l h_vmem=1.9G,s_vmem=1.5G -S /bin/bash -t 1-${ntasks} /home/arosen/pncReproc2015Scripts/antsCT/runCombineWrapperGMDUnion.sh ${joblist}
+qsub -q all.q,basic.q -l h_vmem=1.9G,s_vmem=1.5G -S /bin/bash -t 1-${ntasks} /home/arosen/pncReproc2015Scripts/antsCT/postProc/runCombineWrapperGMDUnion.sh ${joblist}
