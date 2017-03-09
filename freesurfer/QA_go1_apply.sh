fea078e135677e2868789d26a536180069d276b6
megq <megq@upenn.edu>
2017-03-09 09:35:22 -0500

    took out commented out lines

--- freesurfer/QA_go1_apply.sh
+++ freesurfer/QA_go1_apply.sh
@@ -30,9 +30,9 @@
 ########################################
 
 #Set variables
-#slist=/data/joy/BBL/studies/pnc/subjectData/freesurfer/go1_go2_freesurfer53_qa_run_list.txt
-#slist=/data/joy/BBL/studies/pnc/subjectData/freesurfer/go1_go2_go3_freesurfer53_qa_run_list.txt
-slist=/data/joy/BBL/studies/pnc/subjectData/freesurfer/go1_go2_go3_freesurfer53_qa_run_list_n2416.txt
+#slist=/data/joy/BBL/studies/pnc/subjectData/freesurfer/go1_go2_go3_freesurfer53_qa_run_list_n2496.txt
+#slist=/data/joy/BBL/studies/pnc/subjectData/freesurfer/go1_go2_go3_freesurfer53_qa_run_list_n2416.txt
+slist=/data/joy/BBL/studies/pnc/subjectData/freesurfer/go_freesurfer53_qa_run_list_n1601.txt
 export output_dir=/data/joy/BBL/projects/pncReproc2015/freesurfer/stats5_3
 export SUBJECTS_DIR=/data/joy/BBL/studies/pnc/processedData/structural/freesurfer53
 export QA_TOOLS=/data/joy/BBL/applications/QAtools_v1.1/
@@ -43,7 +43,7 @@ export PATH=$FREESURFER_HOME/bin/:$PATH
 sdthresh=2
 
 #give it the subject n that you wish to be appended to each file name
-subjnum="n2416"
+subjnum="n1601"
 
 #a list of go1 subjects so can use for flagging outliers based on go1
 go1_list=/data/joy/BBL/projects/pncReproc2015/antsCT/n1601_bblid_scanid_dateid.csv
@@ -55,27 +55,27 @@ t1_qa=/data/joy/BBL/studies/pnc/n1601_dataFreeze/neuroimaging/t1struct/n1601_t1Q
 demos=/data/joy/BBL/studies/pnc/subjectData/n1601_go1_datarel_073015.csv
 
 ##### 1) Create subcortical volume segmentation csv - aseg stats##### 
-#if [ ! -e "$output_dir/aseg.stats" ]; then
-#	mkdir -p $output_dir/aseg.stats
-#fi
-#asegstats2table --subjectsfile=$slist -t $output_dir/aseg.stats/"$subjnum"_aseg.stats.volume.csv -m volume --skip
+if [ ! -e "$output_dir/aseg.stats" ]; then
+	mkdir -p $output_dir/aseg.stats
+fi
+asegstats2table --subjectsfile=$slist -t $output_dir/aseg.stats/"$subjnum"_aseg.stats.volume.csv -m volume --skip
 
 ##### 2) Create mean QA data charts (thickness and surface area charts)##### 
-#/home/mquarmley/pncReproc2015Scripts/freesurfer/aparc.stats.meanthickness.totalarea.sh $slist $output_dir $SUBJECTS_DIR $subjnum
+/home/mquarmley/pncReproc2015Scripts/freesurfer/aparc.stats.meanthickness.totalarea.sh $slist $output_dir $SUBJECTS_DIR $subjnum
 
 ##### 3) Create parcellation csv's - aparc stats##### 
-#aparcstats2table --hemi lh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_lh.aparc.stats.thickness.csv -m thickness --skip
-#aparcstats2table --hemi rh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_rh.aparc.stats.thickness.csv -m thickness --skip
-#aparcstats2table --hemi lh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_lh.aparc.stats.volume.csv -m volume --skip
-#aparcstats2table --hemi rh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_rh.aparc.stats.volume.csv -m volume --skip
-#aparcstats2table --hemi lh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_lh.aparc.stats.area.csv --skip
-#aparcstats2table --hemi rh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_rh.aparc.stats.area.csv --skip
+aparcstats2table --hemi lh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_lh.aparc.stats.thickness.csv -m thickness --skip
+aparcstats2table --hemi rh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_rh.aparc.stats.thickness.csv -m thickness --skip
+aparcstats2table --hemi lh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_lh.aparc.stats.volume.csv -m volume --skip
+aparcstats2table --hemi rh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_rh.aparc.stats.volume.csv -m volume --skip
+aparcstats2table --hemi lh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_lh.aparc.stats.area.csv --skip
+aparcstats2table --hemi rh --subjectsfile=$slist -t $output_dir/aparc.stats/"$subjnum"_rh.aparc.stats.area.csv --skip
 
 ##### 4) Create CNR and Euler Numbers csv's##### 
-#/home/mquarmley/pncReproc2015Scripts/freesurfer/cnr_euler_number_calculation.sh $slist $SUBJECTS_DIR $output_dir $subjnum
+/home/mquarmley/pncReproc2015Scripts/freesurfer/cnr_euler_number_calculation.sh $slist $SUBJECTS_DIR $output_dir $subjnum
 
 ##### 5) Flag subjects based on whether they are an outlier (>2 SD) on several measures##### 
-#/share/apps/R/R-3.2.3/bin/R --slave --file=/home/mquarmley/pncReproc2015Scripts/freesurfer/flag_outliers_go1_apply.R --args $output_dir $go1_list $sdthresh $t1_qa $subjnum
+/share/apps/R/R-3.2.3/bin/R --slave --file=/home/mquarmley/pncReproc2015Scripts/freesurfer/flag_outliers_go1_apply.R --args $output_dir $go1_list $sdthresh $t1_qa $subjnum
 
 ##### 6) Flag  (gray/csf flag, gray/white flag, euler number flag, number outliers rois thickness flag, total outliers)##### 
 /share/apps/R/R-3.2.3/bin/R --slave --file=/home/mquarmley/pncReproc2015Scripts/freesurfer/cnr_euler_qa_go1_apply.R --args $output_dir $go1_list $t1_qa $subjnum $demos
