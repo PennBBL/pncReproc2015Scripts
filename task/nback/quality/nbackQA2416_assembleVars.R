7d5b49699530d6fe9f601faa00543fa2d3b3cae0
Rastko Ciric <rciric@chead.uphs.upenn.edu>
2017-03-16 21:12:07 -0400

    20170317: quality variable assembler script

---
+++ task/nback/quality/nbackQA2416_assembleVars.R
@@ -0,0 +1,87 @@
+#!/usr/bin/env Rscript
+
+###################################################################
+#  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  ☭  #
+###################################################################
+
+###################################################################
+# Merge the extant data frames
+###################################################################
+
+data <- read.csv('/data/joy/BBL/studies/pnc/subjectData/n2416_pnc_protocol_validation_params_status_20170105.csv')
+xcp <- read.csv('/data/joy/BBL/projects/pncReproc2015/nback/quality/NBACK_XCP.csv')
+b0 <- read.csv('/data/joy/BBL/projects/pncReproc2015/nback/quality/B0MAPCORRECTION.csv')
+maxRelRMS <- read.csv('/data/joy/BBL/projects/pncReproc2015/nback/quality/RELRMS.csv')
+activation <- read.csv('/data/joy/BBL/projects/pncReproc2015/nback/quality/ACTIVATION.csv')
+coverage <- read.csv('/data/joy/BBL/projects/pncReproc2015/nback/quality/NBACK_COVERAGE_PROCESSED2416.csv')
+data8 <- merge(data,coverage,by='scanid',all.x=T)
+data9 <- merge(data8,b0,by='scanid',all.x=T)
+data0 <- merge(data9,xcp,by='scanid',all.x=T)
+data1 <- merge(data0,maxRelRMS,by='scanid',all.x=T)
+data2 <- merge(data1,activation,by='scanid',all.x=T)
+
+###################################################################
+# Determine exclusion criteria
+###################################################################
+data2$nbackMeanRelRMSMotionExclude <- as.numeric(data2$rel_mean_rms_motion > 0.5)
+data2$nbackMaxRelRMSMotionExclude <- as.numeric(data2$nbackMaxRelRMS > 6)
+
+data2$nbackExclude <- as.numeric(!data2$nbackProtocolValidationStatus  | data2$nbackMeanRelRMSMotionExclude | data2$nbackMaxRelRMSMotionExclude | data2$nbackMeanActivationExclude)
+data2$nbackExcludeVoxelwise <- as.numeric(!data2$nbackProtocolValidationStatus | data2$nbackMeanRelRMSMotionExclude | data2$nbackMaxRelRMSMotionExclude | data2$nbackMeanActivationExclude | !data2$nbackVoxelwiseCoverageInclude)
+
+###################################################################
+# Generate the final quality file.
+###################################################################
+
+nbackQA <- data.frame(
+   bblid=data2$bblid.x,
+   scanid=data2$scanid,
+   nbackExclude=data2$nbackExclude,
+   nbackExcludeVoxelwise=data2$nbackExcludeVoxelwise,
+   nbackNoDataExclude=as.numeric(!data2$nbackProtocolValidationStatus) ,
+   nbackRelMeanRMSMotion=data2$rel_mean_rms_motion,
+   nbackRelMeanRMSMotionExclude=data2$nbackMeanRelRMSMotionExclude,
+   nbackRelMaxRMSMotion=data2$nbackMaxRelRMS,
+   nbackRelMaxRMSMotionExclude=data2$nbackMaxRelRMSMotionExclude,
+   nbackNormCrossCorr=data2$normCrossCorr,
+   nbackNormCoverage=data2$normCoverage,
+   nbackCoregCrossCorr=data2$coregCrossCorr,
+   nbackCoregCoverage=data2$coregCoverage,
+   nbackVoxelwiseCoverageExclude=as.numeric(!data2$nbackVoxelwiseCoverageInclude),
+   nbackMeanActivationExclude=data2$nbackMeanActivationExclude,
+   nbackRpsMapCorrectionNotApplied=data2$B0MapUsable
+)
+
+write.csv(nbackQA,'/data/joy/BBL/projects/pncReproc2015/nback/quality/NBACK_QA_2416.csv',row.names=F)
+
+###################################################################
+# Generate a Venn partition.
+###################################################################
+require(limma)
+qux <- data.frame(
+   nbackQA$nbackNoDataExclude,
+   nbackQA$nbackVoxelwiseCoverageExclude,
+   nbackQA$nbackMeanActivationExclude,
+   nbackQA$nbackRelMaxRMSMotionExclude,
+   nbackQA$nbackRelMeanRMSMotionExclude
+)
+baz <- vennCounts(qux)
+baz <- baz[baz[,6]!=0,]
+write.csv(baz,'/data/joy/BBL/projects/pncReproc2015/nback/quality/vennDiagram2416.csv')
+
+###################################################################
+# Cross-sectional subsample
+###################################################################
+data <- read.csv('/data/joy/BBL/studies/pnc/subjectData/n1601_go1_datarel_020716.csv')
+nbackQA1601 <- nbackQA[which(nbackQA$scanid %in% intersect(nbackQA$scanid,data$scanid)),]
+write.csv(nbackQA1601,'/data/joy/BBL/projects/pncReproc2015/nback/quality/NBACK_QA_1601.csv',row.names=F)
+qux <- data.frame(
+   nbackQA1601$nbackNoDataExclude,
+   nbackQA1601$nbackVoxelwiseCoverageExclude,
+   nbackQA1601$nbackMeanActivationExclude,
+   nbackQA1601$nbackRelMaxRMSMotionExclude,
+   nbackQA1601$nbackRelMeanRMSMotionExclude
+)
+baz <- vennCounts(qux)
+baz <- baz[baz[,6]!=0,]
+write.csv(baz,'/data/joy/BBL/projects/pncReproc2015/nback/quality/vennDiagram1601.csv')
