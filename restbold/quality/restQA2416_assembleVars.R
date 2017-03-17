f99cde649bce6cc397b3c91bd8e139972e65f383
Rastko Ciric <rciric@chead.uphs.upenn.edu>
2017-03-16 21:03:13 -0400

    20170317: update scripts after generation of data for release

--- restbold/quality/restQA2416_assembleVars.R
+++ restbold/quality/restQA2416_assembleVars.R
@@ -11,7 +11,7 @@
 data <- read.csv('/data/joy/BBL/studies/pnc/subjectData/n2416_pnc_protocol_validation_params_status_20170105.csv')
 xcp <- read.csv('/data/joy/BBL/projects/pncReproc2015/restbold/quality/REST_XCP.csv')
 b0 <- read.csv('/data/joy/BBL/projects/pncReproc2015/restbold/quality/B0MAPCORRECTION.csv')
-coverage <- read.csv('/data/joy/BBL/projects/pncReproc2015/restbold/quality/REST_COVERAGE_PROCESSED.csv')
+coverage <- read.csv('/data/joy/BBL/projects/pncReproc2015/restbold/quality/REST_COVERAGE_PROCESSED2416.csv')
 data0 <- merge(data,coverage,by='scanid',all.x=T)
 data1 <- merge(data0,b0,by='scanid',all.x=T)
 data2 <- merge(data1,xcp,by='scanid',all.x=T)
@@ -52,7 +52,7 @@ for (i in newSeq) {
    restQA$restNoDataExclude[which(data2$scanid==i)] = 0
 }
 
-write.csv(restQA,'/data/joy/BBL/projects/pncReproc2015/restbold/quality/REST_QA.csv',row.names=F)
+write.csv(restQA,'/data/joy/BBL/projects/pncReproc2015/restbold/quality/REST_QA_2416.csv',row.names=F)
 
 ###################################################################
 # Generate a Venn partition.
@@ -66,4 +66,21 @@ qux <- data.frame(
 )
 baz <- vennCounts(qux)
 baz <- baz[baz[,5]!=0,]
-write.csv(baz,'/data/joy/BBL/projects/pncReproc2015/restbold/quality/vennDiagram.csv')
+write.csv(baz,'/data/joy/BBL/projects/pncReproc2015/restbold/quality/vennDiagram2416.csv')
+
+###################################################################
+# Cross-sectional subsample
+###################################################################
+data <- read.csv('/data/joy/BBL/studies/pnc/subjectData/n1601_go1_datarel_020716.csv')
+restQA1601 <- restQA[which(restQA$scanid %in% intersect(restQA$scanid,data$scanid)),]
+write.csv(restQA1601,'/data/joy/BBL/projects/pncReproc2015/restbold/quality/REST_QA_1601.csv',row.names=F)
+qux <- data.frame(
+   restQA1601$restNoDataExclude,
+   restQA1601$restVoxelwiseCoverageExclude,
+   restQA1601$restNSpikesMotionExclude,
+   restQA1601$restRelMeanRMSMotionExclude
+)
+baz <- vennCounts(qux)
+baz <- baz[baz[,5]!=0,]
+write.csv(baz,'/data/joy/BBL/projects/pncReproc2015/restbold/quality/vennDiagram1601.csv')
+
