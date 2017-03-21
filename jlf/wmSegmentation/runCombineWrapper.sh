f0c8c606c6aba27c26a5dc301b73301f30eced9d
Adon Rosen <arosen@chead.uphs.upenn.edu>
2017-03-21 08:55:04 -0400

    Updated WM segmentation scripts to now err out if outputimage exists

--- jlf/wmSegmentation/runCombineWrapper.sh
+++ jlf/wmSegmentation/runCombineWrapper.sh
@@ -5,4 +5,4 @@ scanid=`echo ${subj} | cut -f 2 -d ,`
 dateid=`echo ${subj} | cut -f 3 -d ,`
 inputImage="/data/joy/BBL/studies/pnc/processedData/structural/jlf/${bblid}/${dateid}x${scanid}/${bblid}_${dateid}x${scanid}_jlfLabels.nii.gz"
 outputImage="/data/joy/BBL/studies/pnc/processedData/structural/jlf/${bblid}/${dateid}x${scanid}/${bblid}_${dateid}x${scanid}_jlfWmSeg.nii.gz"
-/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/jlf/wmSegmentation/createDistanceMetricsMask.sh ${inputImage} ${outputImage}
+/home/arosen/pncReproc2015Scripts/jlf/wmSegmentation/createDistanceMetricsMask.sh ${inputImage} ${outputImage}
--- jlf/wmSegmentation/submitWrapper.sh
+++ jlf/wmSegmentation/submitWrapper.sh
@@ -1,4 +1,4 @@
 joblist="/home/arosen/tempCohortListSplit/cohort_list.csv"
 ntasks=$(cat ${joblist} | wc -l)
 
-qsub -q all.q,basic.q -l h_vmem=5.5G,s_vmem=5.5G -S /bin/bash -t 1-${ntasks} /home/arosen/runCombineWrapper.sh ${joblist}
+qsub -q all.q,basic.q -l h_vmem=5.5G,s_vmem=5.5G -S /bin/bash -t 1-${ntasks} /home/arosen/pncReproc2015Scripts/jlf/wmSegmentation/runCombineWrapper.sh ${joblist}
