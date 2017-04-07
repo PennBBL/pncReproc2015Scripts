3bc5a75def9538f02c08dbd9e81bc95323019259
Adon Rosen <arosen@chead.uphs.upenn.edu>
2017-04-07 14:32:56 -0400

    Added CT grasp file prep script

---
+++ antsCT/prepareValues/prepAntsCTGraspOutput.R
@@ -0,0 +1,38 @@
+# AFGR August 5 2016
+
+##Usage##
+# This script is going to be used to prepare the antsCT JLF data
+# Its going to rm extra columns and then prepare the data's header
+# Might make this into one big function but we will see how that goes =/
+
+# Load data
+source('/home/arosen/adroseHelperScripts/R/afgrHelpFunc.R')
+columnValues <- read.csv("/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/jlf/labelList/inclusionCheck.csv")
+# ctValues was created by using the command found below:
+system("$XCPEDIR/utils/combineOutput -p /data/joy/BBL/studies/pnc/processedData/structural/antsCorticalThickness/ -f graspPNC227_antsCT_val.1D  -o antsCT_grasp227_vals.1D")
+system("mv /data/joy/BBL/studies/pnc/processedData/structural/antsCorticalThickness/antsCT_grasp227_vals.1D /data/joy/BBL/projects/pncReproc2015/antsCT/grasp227AntsCTVals.1D") 
+ctValues <- read.table("/data/joy/BBL/projects/pncReproc2015/antsCT/grasp227AntsCTVals.1D", header=T)
+n1601.subjs <- read.csv('/data/joy/BBL/projects/pncReproc2015/antsCT/n1601_bblid_scanid_dateid.csv')
+n1601.subjs <- n1601.subjs[,c(2,1)]
+
+# Now I need to limit it to just the NZmeans 
+nzCols <- grep('NZMean', names(ctValues))
+nzCols <- append(c(1, 2), nzCols)
+
+ctValues <- ctValues[,nzCols]
+
+# Now change their names
+columnNames <- paste('mprage_grasp227_ct_roi', seq(1,226), sep='')
+colnames(ctValues)[3:length(ctValues)] <- as.character(columnNames)
+
+# Now order and rename the files
+ctValues[,2] <- strSplitMatrixReturn(ctValues$subject.1., 'x')[,2]
+colnames(ctValues)[1:2] <- c('bblid', 'scanid')
+
+# Now write the csv
+write.csv(ctValues, '/data/joy/BBL/projects/pncReproc2015/antsCT/jlfAntsValuesCT.csv', quote=F, row.names=F)
+write.csv(ctValues, paste('/data/joy/BBL/studies/pnc/n2416_dataFreeze/neuroimaging/t1struct/n2416_jlfAntsCTIntersectionCt_',format(Sys.Date(), format="%Y%m%d"),'.csv', sep=''), quote=F, row.names=F)
+
+# Now do the n1601 specific csv
+n1601.ct.vals <- merge(n1601.subjs, ctValues, by=c('bblid', 'scanid'))
+write.csv(n1601.ct.vals, '/data/joy/BBL/studies/pnc/n1601_dataFreeze/neuroimaging/t1struct/n1601_jlfAntsCTIntersectionCt.csv', quote=F, row.names=F)
