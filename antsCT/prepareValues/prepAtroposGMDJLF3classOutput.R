1fb89d765de0a369d4d43b19982f19be66ef67f9
Adon Rosen <adrose@users.noreply.github.com>
2017-04-10 10:17:26 -0400

    Updated GMD output csv name

--- antsCT/prepareValues/prepAtroposGMDJLF3classOutput.R
+++ antsCT/prepareValues/prepAtroposGMDJLF3classOutput.R
@@ -37,8 +37,7 @@ gmdValues[,2] <- strSplitMatrixReturn(gmdValues$subject.1., 'x')[,2]
 colnames(gmdValues)[1:2] <- c('bblid', 'scanid')
 
 # Now write the csv
-write.csv(gmdValues, '/data/joy/BBL/projects/pncReproc2015/antsCT/jlfAntsValuesGMD.csv', quote=F, row.names=F)
-write.csv(gmdValues, paste('/data/joy/BBL/studies/pnc/n2416_dataFreeze/neuroimaging/t1struct/n2416_jlfAntsCTIntersectionGMD_',format(Sys.Date(), format="%Y%m%d"),'.csv', sep=''), quote=F, row.names=F)
+write.csv(gmdValues, paste('/data/joy/BBL/studies/pnc/n2416_dataFreeze/neuroimaging/t1struct/n2416_jlfAtroposIntersectionGMD_',format(Sys.Date(), format="%Y%m%d"),'.csv', sep=''), quote=F, row.names=F)
 
 # Now prepare the n1601 output csv
 n1601.gmd.values <- merge(n1601.subjs, gmdValues, by=c('bblid', 'scanid'))
